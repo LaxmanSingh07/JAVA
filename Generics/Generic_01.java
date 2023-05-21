e478e23d6639ce7edae49f4eedcb58ba2bfd6c79
LAXMAN <laxsingh9659@gmail.com>
2023-05-21 13:05:03 +0530

    Started Generics

--- Generics/Generic_01.java
+++ Generics/Generic_01.java
@@ -2,6 +2,14 @@ class Generic_01
 {
     public static void main(String [] args)
     {
-        Object obj=new String("Hwllo")
+        Object obj=new String("Hello");
+        // String str=obj; // you can't do it like this
+
+        //you have to do the type cast 
+    
+        String str=(String)obj;
+        System.out.println(str);
+
+        
     }
 }
